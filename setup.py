1144cd1e80229498080833d1bea5a007b7a31d2f
Julien Crambes <71984039+Elektriman@users.noreply.github.com>
2023-01-20 18:33:53 +0100

    finishing the automatic setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 import os
+import subprocess
 
 print("initialising virtual environment")
 
-os.system(r"python -m venv venv")
-os.system(r"venv\Scripts\activate")
-os.system(r"pip install -r requirements.txt")
+subprocess.run(r"python -m venv venv && venv\Scripts\activate && pip install -r requirements.txt", shell=True)
 
 print("virtual environment installed")
 
@@ -18,17 +17,18 @@ print("fetching csv data")
 if not os.path.exists('csv_data'):
     os.makedirs('csv_data')
 
-with open("Projet+Python_Dataset_Edstats_csv.zip", "wb") as f :
-    f.write(requests.get(url, allow_redirects=True).content)
+    with open("Projet+Python_Dataset_Edstats_csv.zip", "wb") as f :
+        f.write(requests.get(url, allow_redirects=True).content)
+
+    with zipfile.ZipFile("Projet+Python_Dataset_Edstats_csv.zip", 'r') as zip_ref:
+        zip_ref.extractall("csv_data")
+        info = zipfile.ZipInfo("csv_data")
+        info.external_attr = 777
+
+    os.remove(r"Projet+Python_Dataset_Edstats_csv.zip")
 
 print("csv data recovered")
 
-with zipfile.ZipFile("Projet+Python_Dataset_Edstats_csv.zip", 'r') as zip_ref:
-    zip_ref.extractall("csv_data")
-
-os.remove(r"Projet+Python_Dataset_Edstats_csv.zip")
-
 print("launch jupyter notebook")
 
-os.system(r"venv\Scripts\activate")
-os.system(r'jupyter notebook "Projet 2 OpenClassrooms.ipynb"')
+subprocess.run(r'venv\Scripts\activate && jupyter notebook Projet_2_OpenClassrooms.ipynb', shell=True)
